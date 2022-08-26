a4fc3244c2d04c92b5e3dfea81622dc1cf45007f
DPMachineLearning <50521222+DPMachineLearning@users.noreply.github.com>
2022-08-26 19:11:46 +0800

    Add files via upload

---
+++ multiple ensembles/Regression/AI4I.py
@@ -0,0 +1,21 @@
+from self_tool import metrics_science
+
+metrics_science.run_experiment(core_str='AI4I',
+                               xgb=True,
+                               kFold=True,
+                               pri_list=[1],
+                               all_tree=[100, 150, 200, 250, 300],
+                               k=36.4, b=40.2)
+
+
+
+
+
+
+
+
+
+
+
+
+
